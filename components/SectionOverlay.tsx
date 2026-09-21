d9c62174bd1d2c6d294654315ea78fa8b28c63bc
joeljarvi <joeljarvi@live.se>
2026-09-21 08:47:43 +0200

    push

--- components/SectionOverlay.tsx
+++ components/SectionOverlay.tsx
@@ -16,7 +16,7 @@ export default function SectionOverlay({
         type="button"
         onClick={onClick}
         aria-label={`Show ${section}`}
-        className={`group/overlay  absolute z-900 inset-0 bg-background/90 hover:bg-background/80 backdrop-blur-xs transition-all duration-700 ease-out flex items-start justify-center cursor-pointer ${
+        className={`group/overlay  absolute z-900 inset-0 bg-background/60 hover:bg-background/70 backdrop-blur-xs transition-all duration-700 ease-out flex items-start justify-center cursor-pointer ${
           dismissed ? "opacity-0 pointer-events-none" : "opacity-100"
         }`}
       >
